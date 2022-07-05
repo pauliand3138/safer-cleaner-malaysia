cc6588e827708b4d190b7277cfad4cbbc592744f
Paul <paul.iand3138@hotmail.com>
2022-07-05 10:55:59 +0800

    Add postMessage model and fetch posts from database

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,25 @@
-export const getPosts = (req, res) => {
-    res.send('THIS WORKS!');
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.json(postMessages);
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+
+        res.json(newPost);
+    } catch (error) {
+        res.json({ message: error.message });
+    }
 }
