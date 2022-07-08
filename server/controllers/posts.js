da7a722100d8daad699acd9dcfb3df35ff77b9ea
Paul <paul.iand3138@hotmail.com>
2022-07-08 15:42:27 +0800

    Delete post functionality server side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -31,7 +31,18 @@ export const updatePost = async (req, res) => {
 
     if (!mongoose.Types.ObjectId.isValid(_id)) return res.send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id}, { new: true });
 
     res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.send('No post with that id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully' });
 }
